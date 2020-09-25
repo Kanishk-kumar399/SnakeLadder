0eb36583ed3b31168f7193660b7573319aea4035
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-25 22:43:28 +0530

    Added UC2 file to get the dice roll

---
+++ Desktop/capgemini/SnakeLadder/SnakeLadderUC2.java
@@ -0,0 +1,9 @@
+public class SnakeLadderUC2
+{
+	public static void main(String args[])
+	{
+	int  PlayerPosition=0;
+	int DiceRoll=(int)(Math.random())%6+1;
+	System.out.print("The Dice Roll Gives :"+DiceRoll);
+	}
+}
