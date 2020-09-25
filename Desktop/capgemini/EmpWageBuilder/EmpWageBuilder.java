2f392d766984d1f4a64e99847961178d233f4137
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-25 17:34:00 +0530

    Added The UC6 for Full Wage calculation with condition

--- Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
+++ Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
@@ -3,15 +3,17 @@ public class EmpWageBuilder
  	 static final int PARTTIME=1;
 	 static final int FULLTIME=2;
          static final int EMP_RATE_PER_HOUR=20;
-	 static final int NUMBER_OF_WORKDAYS=2;
+	 static final int NUMBER_OF_WORKDAYS=20;
+	 static final int MAXIMUM_WORKING_HOURS=100;
        	 public static void main(String args[])
 	 {
 	  //variables
 	  int empHours=0;
-	  int empWage=0;
-	  int totalWage=0;
-	  for(int day=0;day<NUMBER_OF_WORKDAYS;day++)
+	  int totalHours=0;
+	  int totalWorkingDays=0;
+	  while(totalHours<=MAXIMUM_WORKING_HOURS && totalWorkingDays<NUMBER_OF_WORKDAYS)
 	   {
+	    totalWorkingDays++;
  	    int empCheck=(int)Math.floor(Math.random()*10)%3;
 	    switch(empCheck)
 	      {
@@ -24,10 +26,10 @@ public class EmpWageBuilder
 		default:
 	 	     empHours=0;
 	      }
-	    empWage=empHours*EMP_RATE_PER_HOUR;
-	    totalWage+=empWage;
-	    System.out.println("Employee Wage :"+empWage);
+	    totalHours+=empHours;
+	    System.out.println("Days :"+totalWorkingDays+" Hours :"+empHours);
 	   }
+	    int totalWage=totalHours*EMP_RATE_PER_HOUR;
 	   System.out.println("Total Employee Wage :"+totalWage);
          }
 }
