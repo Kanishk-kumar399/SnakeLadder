a753ce1d7f0c94eeef67a6de8c5a37e8a33cfaa6
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-25 16:51:08 +0530

    Added UC3 for part time and Wage calculation

--- Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
+++ Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
@@ -3,16 +3,19 @@ public class EmpWageBuilder
 	public static void main(String args[])
 	{
 	 //constants
-	 int FULLTIME=1;
+	 int PARTTIME=1;
+	 int FULLTIME=2;
 	 int EMP_RATE_PER_HOUR=20;
 	 //variables
 	 int empHours=0;
 	 int empWage=0;
- 	 int empCheck=(int)(Math.random()*10)%2;
+ 	 int empCheck=(int)(Math.random()*10)%3;
 	 if(empCheck==FULLTIME)
 		empHours=8;
+	 else if(empCheck==PARTTIME)
+	        empHours=4;
 	 else
-	        empHours=0;
+	 	empHours=0;
 	 empWage=empHours*EMP_RATE_PER_HOUR;
 	 System.out.println("Employee Wage :"+empWage);
 	}
