06cddd0ff57da0d6fd8cc230bb101c4fb609262b
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-25 22:16:09 +0530

    Added UC1 File for UseCase1

---
+++ Desktop/capgemini/SnakeLadder/SnakeLadder.java
@@ -0,0 +1,8 @@
+public class SnakeLadder
+{
+	public static void main(String args[])
+	{
+	int  PlayerPosition=0;
+	System.out.println("The Position of Player in Starting is :"+PlayerPosition);
+	}
+}
