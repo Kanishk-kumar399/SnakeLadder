f9a7ad1e7622ee5a9749e4b26b3e7ea12d06b4a2
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-25 17:20:09 +0530

    Adding UC5 for Calculating Wage of Month

--- Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
+++ Desktop/capgemini/EmpWageBuilder/EmpWageBuilder.java
@@ -1,26 +1,34 @@
 public class EmpWageBuilder
 {
-      	 final static int PARTTIME=1;
-	 final static int FULLTIME=2;
-	 final static int EMP_RATE_PER_HOUR=20;
-	public static void main(String args[])
-	{
-	 //variables
-	 int empHours=0;
-	 int empWage=0;
- 	 int empCheck=(int)Math.floor(Math.random()*10)%3;
-	 switch(empCheck)
+ 	 static final int PARTTIME=1;
+	 static final int FULLTIME=2;
+         static final int EMP_RATE_PER_HOUR=20;
+	 static final int NUMBER_OF_WORKDAYS=2;
+       	 public static void main(String args[])
+	 {
+	  //variables
+	  int empHours=0;
+	  int empWage=0;
+	  int totalWage=0;
+	  for(int day=0;day<NUMBER_OF_WORKDAYS;day++)
+	   {
+ 	    int empCheck=(int)Math.floor(Math.random()*10)%3;
+	    switch(empCheck)
 	      {
 		case FULLTIME:
-		empHours=8;
-		break;
+	       	     empHours=8;
+		     break;
 	  	case PARTTIME:
-	        empHours=4;
-		break;
+	             empHours=4;
+		     break;
 		default:
-	 	empHours=0;
+	 	     empHours=0;
 	      }
-	 empWage=empHours*EMP_RATE_PER_HOUR;
-	 System.out.println("Employee Wage :"+empWage);
-	}
+	    empWage=empHours*EMP_RATE_PER_HOUR;
+	    totalWage+=empWage;
+	    System.out.println("Employee Wage :"+empWage);
+	   }
+	   System.out.println("Total Employee Wage :"+totalWage);
+         }
 }
+
